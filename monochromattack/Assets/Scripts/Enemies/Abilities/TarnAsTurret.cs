206ae6a201a28c7ffc0e13eb78e9090b658fe7b0
Twinfox <bastian.7741@gmail.com>
2019-08-04 00:00:06 +0200

    Implemented blue enemy.

---
+++ monochromattack/Assets/Scripts/Enemies/Abilities/TarnAsTurret.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TarnAsTurret : MonoBehaviour, Ability
+{
+    public GameObject normalSprite;
+    public GameObject turretSprite;
+    public float tarnDuration = 1f;
+    public float waitDuration = 0.5f;
+
+    public IEnumerator Execute (Action callback)
+    {
+        normalSprite.SetActive(false);
+        turretSprite.SetActive(true);
+
+        yield return new WaitForSeconds(tarnDuration);
+
+        normalSprite.SetActive(true);
+        turretSprite.SetActive(false);
+
+        yield return new WaitForSeconds(waitDuration);
+
+        callback.Invoke();
+    }
+}
