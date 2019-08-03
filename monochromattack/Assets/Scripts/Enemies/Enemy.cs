17cb376d33b8aaba0511970e7f7ce91ab2a7c77d
Twinfox <bastian.7741@gmail.com>
2019-08-03 21:48:32 +0200

    Enemies now correctly kill players

--- monochromattack/Assets/Scripts/Enemies/Enemy.cs
+++ monochromattack/Assets/Scripts/Enemies/Enemy.cs
@@ -38,4 +38,18 @@ public class Enemy : MonoBehaviour
             StartCoroutine(ability.Execute(() => isBusy = false));
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            var destructable = other.gameObject.GetComponent<Destructable>();
+            if(destructable.IsIndestructable)
+            {
+                return;
+            }
+
+            destructable.Die();
+        }
+    }
 }
